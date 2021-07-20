922ea81036d0e757b1e6d25a47e8b87f31da6568
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:34:53 +0200

    Update a10_p1.cpp

--- Assignment_10/a10_p1.cpp
+++ Assignment_10/a10_p1.cpp
@@ -14,29 +14,3 @@ void Critter::print() {
 int Critter::getHunger() {
     return hunger;
 }
-
-/*
-#include <string> // defines standard C++ string class
-*/
-/* First C++ class */
-/*
-class Critter
-{
-private:  // data members are private
-    int hunger;
-    int boredom;
-    double height;
-	std::string name;
-public: // business logic methods are public
-    // setter methods
-    void setName(std::string& newname) {
-        name = newname;
-    }
-    void setHunger(int newhunger);
-    void setBoredom(int newboredom);
-    // getter method
-    int getHunger();
-    // service method
-    void print();
-};
-*/
