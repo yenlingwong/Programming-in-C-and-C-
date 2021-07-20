38aeb3cbc216cb84a351299b7fb64ff648e2f4db
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:22:54 +0200

    Update testa11_p3.cpp

--- Assignment_11/testa11_p3.cpp
+++ Assignment_11/testa11_p3.cpp
@@ -38,30 +38,3 @@ int main() {
  
     return 0;
 }
-/*#include <iostream>
-#include "Creature.h"
-using namespace std;
-
-int main() {
-    cout << "Creating an Creature.\n";
-    Creature c;
-    c.run();
-
-    cout << "\nCreating a Wizard.\n";
-    Wizard w;
-    w.run();
-    w.hover();
-
-    cout << "\nCreating a Hobbit.\n";
-    Hobbit h;
-    h.run();
-    h.caloriesUsed();
-
-    cout << "\nCreating an Orc.\n";
-    Orc o;
-    o.run();
-    o.punchingPower();
-
-    return 0;
-}
-*/
