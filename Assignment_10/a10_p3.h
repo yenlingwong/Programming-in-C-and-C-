b3f3322870aa1ca6bfa5b2658e3b9ae3498bcf4a
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:33:30 +0200

    Update a10_p3.h

--- Assignment_10/a10_p3.h
+++ Assignment_10/a10_p3.h
@@ -36,29 +36,3 @@ class City {
     // Printing Methods
 
 };
-/*
-#include <string>
-
-class City {
-	private:
-		int population;
-		double area;
-		std::string name;
-		std::string mayor;
-	public:
-		// setter methods
-		void setName(std::string newName);
-		void setArea(double newArea);
-		void setPopulation(int newPopulation);
-		void setMayor(std::string newMayor);
-
-		// getter methods
-		std::string getName();
-		double getArea();
-		int getPopulation();
-		std::string getMayor();
-
-		// service method
-		void print();
-};
-*/
