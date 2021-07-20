d5aef370ce96ea6d1f5aac4390d86a6c48feb7d5
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:18:52 +0200

    Update Vector.h

--- Assignment_11/Vector.h
+++ Assignment_11/Vector.h
@@ -42,30 +42,3 @@ class Vector {
         double product(const Vector& ) const;
         // Operation Methods
 };
-/*
-#ifndef __VECTOR_H
-#define __VECTOR_H
-class Vector {
-    private:
-        double *ptr;
-        int size;
-    public:
-        Vector(double *Ptr, int Size);
-        Vector();
-        Vector(const Vector&);
-        ~Vector();
-
-        void setPtr(double *Ptr);
-        void setSize(int Size);
-
-        double* getPtr() const;
-        int getSize() const;
-
-        void printComponents() const;
-        double norm() const;
-        Vector add(const Vector& secAddend) const;
-        Vector subtract(const Vector& subtrahend) const;
-        double scalarProduct(const Vector& second) const;
-};
-#endif
-*/
