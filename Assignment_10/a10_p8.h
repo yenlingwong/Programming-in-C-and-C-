c8d24c95c6fcfbef7aab8718704d67a641283a21
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:27:30 +0200

    Update a10_p8.h

--- Assignment_10/a10_p8.h
+++ Assignment_10/a10_p8.h
@@ -40,39 +40,3 @@ class Complex {
     Complex difference(Complex& copy1);
     Complex multiplication(Complex &copy1);
 };
-/*
-#include <string>
-
-class Complex {
-    private:
-        int real;
-        int imaginary;
-
-    public:
-        // Constructors
-        // Construct Critter object with all parameters supplied
-        Complex(int real, int complex);
-        // Construct Complex with no parameters supplied
-        Complex();
-        // Copy constructor
-        Complex(Complex& num);
-
-        // Destructor
-        ~Complex();
-
-        // setter methods
-        void setReal(int newReal);
-        void setImaginary(int newImaginary);
-
-        // getter methods
-        int getReal();
-        int getImaginary();
-
-        // service method
-        Complex add(Complex& num); 
-        Complex sub(Complex& num); 
-        Complex mul(Complex& num);
-        Complex conjugate();
-        void print();
-};
-*/
