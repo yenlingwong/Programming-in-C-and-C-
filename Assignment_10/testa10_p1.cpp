50dd19fad1fe8fb877047a4f8b99477b30f76d35
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:27:07 +0200

    Update testa10_p1.cpp

--- Assignment_10/testa10_p1.cpp
+++ Assignment_10/testa10_p1.cpp
@@ -30,33 +30,3 @@ int main(int argc, char** argv)
     c.print();
         return 0;
 }
-/*
-#include <iostream>
-#include <cstdlib>
-#include "Critter.h"
- 
-using namespace std;
- 
-int main(int argc, char** argv)
-{
-    Critter c;
- 
-    string name;
-    int hunger;
- 
-    cout << endl << "Please enter data: " << endl;
-    cout << "Name: ";
-    // cin >> name; will not work if name contains
-    // spaces
-    getline(cin, name);
-    c.setName(name);
-    c.setName(name);
-    cout << "Hunger: ";
-    cin >> hunger;
-    c.setHunger(hunger);
- 
-    cout << "You have created:" << endl;
-    c.print();
-        return 0;
-}
-*/
