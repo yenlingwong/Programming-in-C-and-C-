22bda53c171b2b8469c23000bcbaee16fbf6e8f0
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:26:07 +0200

    Update testa10_p3.cpp

--- Assignment_10/testa10_p3.cpp
+++ Assignment_10/testa10_p3.cpp
@@ -53,37 +53,3 @@ int main() {
 
     return 0;
 };
-/*
-#include <iostream>
-#include <cstdlib>
-#include "City.h"
-
-using namespace std;
-
-int main(int argc, char** argv) {
-	City Bremen, Paris, London;
-
-	// Set value for each data member of the three instances
-	Bremen.setName("Bremen");
-	Bremen.setPopulation(569352);
-	Bremen.setMayor("Andreas Bovenschulte");
-	Bremen.setArea(326.73);
-
-	Paris.setName("Paris");
-	Paris.setPopulation(2140526);
-	Paris.setMayor("Anne Hidalgo");
-	Paris.setArea(105.4);
-
-	London.setName("London");
-	London.setPopulation(8908081);
-	London.setMayor("Sadiq Khan");
-	London.setArea(1572);
-
-	// Output the data members of each instance through the public print method
-	Bremen.print();
-	Paris.print();
-	London.print();
-
-	return 0;
-}
-*/
