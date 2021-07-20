a3e479125d8d02015d2c8125c35dc1752c057060
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:25:44 +0200

    Update testa10_p4.cpp

--- Assignment_10/testa10_p4.cpp
+++ Assignment_10/testa10_p4.cpp
@@ -32,33 +32,3 @@ int main(int argc, char** argv){
     
     return 0;
 }
-/*
-#include <iostream>
-#include <cstdlib>
-#include "Critter.h"
-
-using namespace std;
-
-int main(int argc, char **argv) {
-    // Creating Critter Objects and then outputting data members into stdout
-    // First instance using constructor with no parameters supplied
-    Critter A;
-    A.setHunger(2); // Set hunger to 2 using setHunger method
-    A.print();
-
-    // Second instance using constructor with name parameter supplied
-    Critter B("Barney");
-    B.setHunger(2); // Set hunger to 2 using setHunger method
-    B.print();
-
-    // Third instance using constructor with 3 parameters omitting height
-    Critter C("Cupcake", 2, 10);
-    C.print();
-
-    // Fourth instance using constructor with 4 parameters
-    Critter D("Jade", 2, 50, 150.0);
-    D.print();
-    
-    return 0;
-}
-*/
