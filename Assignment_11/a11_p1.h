affe8c02c0126713af8622f5ab0944093be31349
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-20 14:19:45 +0200

    Update a11_p1.h

--- Assignment_11/a11_p1.h
+++ Assignment_11/a11_p1.h
@@ -42,35 +42,3 @@ class Box {
     // Printing Method
 
 };
-/*
-class Box {
-    private:
-        double height;
-        double width;
-        double depth;
-
-    public:
-        // Constructors
-        // Construct Box with all parameters
-        Box(double Height, double Width, double Depth);
-        // Default Construct Box with no parameters
-        Box();
-        // Copy constructor
-        Box(Box& box);
-        // Copy operator
-        Box& operator=(const Box& op) = default;
-
-        // Destructor
-        ~Box();
-
-        // setter methods
-        void setH(double Height);
-        void setW(double Width);
-        void setD(double Depth);
-
-        // getter methods
-        double getH() const;
-        double getW() const;
-        double getD() const;
-};
-*/
