2fc9350a3a6af83140e463c890de6b722323b09e
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:33:10 +0200

    Update a3_p6.c

--- Assignment_3/a3_p6.c
+++ Assignment_3/a3_p6.c
@@ -31,31 +31,3 @@ int main() {
 }
 
 //Function Declaration
-
-
-/*
-#include<stdio.h>
-
-// Implementation of conversion from kg and g to pounds
-float to_pounds(int kg, int g) {
-    float pounds;
-    pounds = (kg * 2.2) + (g * 0.0022);
-    return pounds;
-}
-
-int main() {
-    int kg, g;
-    float pounds;
-
-    scanf("%d", &kg);
-    getchar();
-    scanf("%d", &g);
-
-    // Assign return value of to_pounds() to float variable pounds
-    pounds = to_pounds(kg, g);
-
-    printf("Result of conversion: %f\n", pounds);
-    
-    return 0;
-}
-*/
