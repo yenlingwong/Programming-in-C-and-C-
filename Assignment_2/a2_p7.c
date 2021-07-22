dd27f3a582f1602ce7c5bbc165878e966f8d38ef
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:39:07 +0200

    Update a2_p7.c

--- Assignment_2/a2_p7.c
+++ Assignment_2/a2_p7.c
@@ -16,20 +16,3 @@ int main() {
     printf("That's it.\n");
     return 0;
 }
-
-/*
-#include <stdio.h>
-
-int main() {
-	int i = 8;
-	
-	// Printf was the only statement that was being iterated over
-	while (i >= 4) { // Placing both statements inside {} so i decrements
-		printf("i is %d\n", i);
-		i--;
-	}
-	printf("That's it.\n");
-	
-	return 0;
-}
-*/
