19d1d23d0086625f50079b628f9613496ba82779
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:37:53 +0200

    Update a2_p10.c

--- Assignment_2/a2_p10.c
+++ Assignment_2/a2_p10.c
@@ -6,7 +6,7 @@ ywong@jacobs-university.de
 */
 
 #include <stdio.h>
-/*
+
 int main() {
     int i;
     int n;
@@ -35,50 +35,3 @@ int main() {
    
     return 0; 
 }
-*/
-
-int main() {
-    int x;
-    int i = 1;
-    scanf("%d", &x);
-
-    while (x <= 0) {
-        scanf("%d",&x);
-    } 
-
-    while(i <= x) {
-        if (i == 1) {
-            printf("%d day = %d hours\n", i , i * 24);
-        } else {
-            printf("%d days = %d hours\n", i , i * 24);
-        }
-        i++;
-    }
-    return 0;
-}
-/*
-#include<stdio.h>
-
-int main() {
-	int n;
-	int i = 1; // Initialize iterating variable
-
-	// Accept input only if n is bigger than 0
-	do {
-		scanf("%d", &n);
-		getchar();
-	} while(n <= 0);
-	
-	
-	while(i != n + 1) { // Iterate until i is n + 1
-		if(i == 1) // Modify output to print "day" instead of "days" when i=1
-			printf("%d day = %d hours\n", i, i * 24);
-		else
-			printf("%d days = %d hours\n", i, i * 24);
-		
-		i = i + 1;
-	}
-	
-	return 0;
-}
-*/
