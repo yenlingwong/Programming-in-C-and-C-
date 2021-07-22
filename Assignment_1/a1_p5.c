48aae3699706917f523262bf58e00fa20052401c
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:42:51 +0200

    Update a1_p5.c

--- Assignment_1/a1_p5.c
+++ Assignment_1/a1_p5.c
@@ -19,21 +19,3 @@ ywong@jacobs-university.de
 
         return 0;
     }
-
-    /*
-    #include<stdio.h>
-int main () {
-	// Declare and initiliaze variables
-    int x = 2138;
-    float y = -52.358925;
-    char z = 'G';
-    double u = 61.295339687;
-    
-    printf("x=%9d\n", x); // Output x over 9 positions
-    printf("y=%11.5f\n", y); // Output y over 11 positions with precision up to 5 digits
-    printf("z=%c\n", z); // Output c as a character
-    printf("u=%.7lf\n", u); // Output u with precision up to 7 digits
-    
-    return 0;
-}
-*/
