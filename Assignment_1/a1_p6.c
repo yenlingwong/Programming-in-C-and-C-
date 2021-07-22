88301eedecdf650bd71d60b826de74aa25039ffa
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:43:08 +0200

    Update a1_p6.c

--- Assignment_1/a1_p6.c
+++ Assignment_1/a1_p6.c
@@ -14,18 +14,3 @@ int main() {
 
     return 0;
 }
-/*
-#include<stdio.h>
-int main() {
-    char c = 'F';
-    
-    // After adding 3 to c, we cast it as a char in order to get the character
-    // that has a value of 3 more than the equivalent one of F in the ASCII
-    printf("%c\n", (char) (c + 3));
-    
-    // Print the corresponding integer value of the character in ASCII
-    printf("%d\n", c + 3);
-    
-    return 0;
-}
-*/
