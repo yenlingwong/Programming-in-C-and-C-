d30c24c1d5578ac0b910fbefa98de21f91c678ae
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:35:17 +0200

    Update a3_p7.c

--- Assignment_3/a3_p7.c
+++ Assignment_3/a3_p7.c
@@ -33,37 +33,3 @@ int main() {
 
     return 0;
 }
-/*
-#include<stdio.h>
-
-void print_form(int n, int m, char c);
-
-int main() {
-    int n, m;
-    char c;
-
-    scanf("%d", &n);
-    getchar();
-
-    scanf("%d", &m);
-    getchar();
-
-    scanf("%c", &c);
-    
-    print_form(n, m, c);
-
-    return 0;
-}
-
-void print_form(int n, int m, char c) {
-    int i, j;
-    // Outer loop for iterating through rows depending on the height n
-    for(i = 0; i < n; i++) {
-        // Inner loop to print character c m+i times in a row
-        for(j = 0; j < (m + i); j++) {
-            printf("%c", c);
-        }
-        printf("\n");
-    }
-}
-*/
