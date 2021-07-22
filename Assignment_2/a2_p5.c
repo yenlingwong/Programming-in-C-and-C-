fb34f52cf0a0ec12babe04631669acf0df1a734f
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:38:39 +0200

    Update a2_p5.c

--- Assignment_2/a2_p5.c
+++ Assignment_2/a2_p5.c
@@ -23,26 +23,3 @@ int main() {
 
     return 0;
 } 
-
-/*
-#include<stdio.h>
-
-int main() {
-	int a;
-	int* ptr_a = &a;
-	
-	scanf("%d", &a);
-	
-	// Print the address that ptr_a has stored
-	printf("%p\n", ptr_a);
-	
-	// Increment the value that ptr_a is pointing to by 5 
-	*ptr_a = *ptr_a + 5;
-	
-	// Print the value by dereferencing ptr_a
-	printf("%d\n", *ptr_a);
-	printf("%p\n", ptr_a);
-	
-	return 0;
-}
-*/
