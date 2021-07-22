e455fdca69ab4b8ab78a3b2ce9eab11d1b102563
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:39:21 +0200

    Update a2_p8.c

--- Assignment_2/a2_p8.c
+++ Assignment_2/a2_p8.c
@@ -21,19 +21,3 @@ int main() {
 
     return 0;
 }
-/*
-#include<stdio.h>
-
-int main() {
-	int number;
-	
-	scanf("%d", &number);
-	
-	// Check if remainder is 0 in both cases when dividing by 2 and 7
-	if((number % 2 == 0) && (number % 7 == 0)) {
-		printf("The number is divisible by 2 and 7\n");
-	} else printf("The number is NOT divisible by 2 and 7\n");
-	
-	return 0;
-}
-*/
