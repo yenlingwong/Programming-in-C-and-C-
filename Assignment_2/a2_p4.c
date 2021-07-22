94b8c9186a20d50885e35d3efb12011167e9cc1f
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:38:25 +0200

    Update a2_p4.c

--- Assignment_2/a2_p4.c
+++ Assignment_2/a2_p4.c
@@ -23,33 +23,3 @@ int main() {
     printf("trapezoid area=%f\n", trapezoid_area);
     return 0;
 }
-
-/*
-#include<stdio.h>
-
-int main() {
-	float a, b, h;
-	
-	// Input bases and heights
-	scanf("%f", &a);
-	getchar();
-	scanf("%f", &b);
-	getchar();
-	scanf("%f", &h);
-	getchar();
-	
-	// Calculate areas and assign them to respective float variables
-	float squareArea = a * a;
-	float rectangleArea = a * b;
-	float triangleArea = 0.5 * (a * h);
-	float trapezoidArea = 0.5 * h * (a + b);
-	
-	// Print areas
-	printf("square area=%f\n", squareArea);
-	printf("rectangle area=%f\n", rectangleArea);
-	printf("triangle area=%f\n", triangleArea);
-	printf("trapezoid area=%f\n", trapezoidArea);
-	
-	return 0;
-}
-*/
