850a6e2593d0530d437f39279b12df385472e9fd
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:06:46 +0200

    Update a9_p1.cpp

--- Assignment_9/a9_p1.cpp
+++ Assignment_9/a9_p1.cpp
@@ -22,17 +22,3 @@ int main () {
 
     return 0 ;
 }
-/*
-#include <iostream>
-
-int main(int argc, char **argv) {
-    std::string country;
-
-    // Input string
-    std::cin >> country;
-    // Output string and end of line
-    std::cout << country << std::endl;
-
-    return 0;
-}
-*/
