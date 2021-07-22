c727fe55d994140ecf9f2281f6536638defdbd7c
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:06:03 +0200

    Update a9_p3.cpp

--- Assignment_9/a9_p3.cpp
+++ Assignment_9/a9_p3.cpp
@@ -31,26 +31,3 @@ int main () {
 
     return 0;
 }
-
-/*
-#include <iostream>
-
-// Function returns a float value, takes a float
-// It returns the absolute value of the input float
-float abs(float number);
-
-int main(int argc, char **argv) {
-    float x;
-
-    std::cin >> x; // Input float
-    std::cout << abs(x); // Output the absolute value of float
-    
-    return 0;
-}
-
-float abs(float number) {
-    if (number >= 0)
-        return number;
-    else return (-number); // Return positive value
-}
-*/
