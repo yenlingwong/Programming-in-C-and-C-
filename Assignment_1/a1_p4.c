f48033216a27bdc8dbf730d0560f512ebb1356bd
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:42:35 +0200

    Update a1_p4.c

--- Assignment_1/a1_p4.c
+++ Assignment_1/a1_p4.c
@@ -29,21 +29,3 @@ ywong@jacobs-university.de
 
         return 0;
     }
-
-    /*
-    #include<stdio.h>
-int main () {
-    int x = 17;
-    int y = 4;
-    
-    printf("x=%d\n", x);
-    printf("y=%d\n", y);
-    printf("sum=%d\n", x+y);
-    printf("product=%d\n", x*y);
-    printf("difference=%d\n", x-y);
-    printf("division=%f\n", (float)x / y);
-    printf("remainder of division=%d\n", x%y);
-    
-    return 0;
-}
-*/
