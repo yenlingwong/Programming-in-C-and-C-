2041e2935e3039c494ed19ae8e29fffb26e4a2bd
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:38:11 +0200

    Update a2_p3.c

--- Assignment_2/a2_p3.c
+++ Assignment_2/a2_p3.c
@@ -26,23 +26,3 @@ int main() {
 
     return 0;
 }
-/*
-#include<stdio.h>
-
-int main() {
-	int weeks, days, hours;
-	
-	// Input number of weeks, days and hours 
-	scanf("%d", &weeks);
-	getchar();
-	scanf("%d", &days);
-	getchar();
-	scanf("%d", &hours);
-	getchar();
-	
-	// Multiply weeks by 168 hours/week and days by 24 hours/day and sum them up
-	printf("%d\n", (weeks * 168 + days * 24 + hours));
-	
-	return 0;
-}
-*/
