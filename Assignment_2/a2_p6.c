1089a658e809933f03ff0c8a95ca542a31613b3e
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:38:53 +0200

    Update a2_p6.c

--- Assignment_2/a2_p6.c
+++ Assignment_2/a2_p6.c
@@ -23,29 +23,3 @@ int main() {
     printf("Address of ptr_three is %p\n", ptr_three);
     return 0;
 }
-
-/*
-#include<stdio.h>
-
-int main() {
-	double x, y;
-	
-	// Initiliazing pointers to x and y
-	double* ptr_one = &x;
-	double* ptr_two = &x;
-	double* ptr_three = &y;
-	
-	// Printing statements
-	printf("Pointer ptr_one contains memory address: %p\n", ptr_one);
-	printf("Pointer ptr_two contains memory address: %p\n", ptr_two);
-	printf("Pointer ptr_three contains memory address: %p\n", ptr_three);	
-	
-	// Check if first two are same and the third one is different
-	if(ptr_one == ptr_two)
-		printf("Pointer ptr_one and pointer ptr_two point to same address.\n");
-	if((ptr_one != ptr_three) && (ptr_two != ptr_three))
-		printf("Pointer ptr_three is different from ptr_one and ptr_two.\n");
-		
-	return 0;
-}
-*/
