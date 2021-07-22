28ec8b49b54016afcb868e1bbe7a56f5a10ff35e
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:28:55 +0200

    Update a3_p1.c

--- Assignment_3/a3_p1.c
+++ Assignment_3/a3_p1.c
@@ -25,32 +25,3 @@ int main() {
     }
     return 0;
 }
-
-/*
-#include<stdio.h>
-
-int main() {
-    float x;
-    int n, i;
-	
-	scanf("%f", &x);
-	getchar();
-
-    scanf("%d", &n);
-    getchar();
-    
-    // Cycle until input for n is a valid integer value
-    while(n <= 0) {
-        printf("Input is invalid, reenter value\n");
-        scanf("%d", &n);
-        getchar();
-    }
-
-	// Print the float n times by iterating from 0 until n (inclusive)
-    for(i = 0; i < n; i++) {
-        printf("%f\n", x);
-    }
-
-    return 0;
-}
-// */
