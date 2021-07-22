74302dba11ebe95493445911ea1b685d03eb7876
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:39:35 +0200

    Update a2_p9.c

--- Assignment_2/a2_p9.c
+++ Assignment_2/a2_p9.c
@@ -22,22 +22,3 @@ int main() {
     }
     return 0;
 }
-/*
-#include<stdio.h>
-#include<ctype.h>
-
-int main() {
-	char c;
-	
-	scanf("%c", &c);
-	
-	if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) // Check if c is letter
-		printf("%c is a letter\n", c);
-	else if (c >= '0' && c <= '9') // Else check if it is a digit
-		printf("%c is a digit\n", c);
-	else 
-		printf("%c is some other symbol\n", c); // Else it is a symbol
-	
-	return 0;
-}
-*/
