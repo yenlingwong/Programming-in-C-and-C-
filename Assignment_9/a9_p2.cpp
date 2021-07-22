9beeb23b87f04663f09eacec06137d63d5e45e98
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:06:19 +0200

    Update a9_p2.cpp

--- Assignment_9/a9_p2.cpp
+++ Assignment_9/a9_p2.cpp
@@ -28,21 +28,3 @@ int main () {
 
     return 0 ;
 }
-/*
-#include <iostream>
-
-int main(int argc, char **argv) {
-    int n;
-    double x;
-    std::string s;
-
-    std::cin >> n >> x >> s;
-    
-    // Loop n times and output string:double every time
-    for (int i = 0; i < n; i++) {
-        std::cout << s << ":" << x << std::endl;
-    }
-    
-    return 0;
-}
-*/
