5040cce9c9f5be525efcd7e443ff43fc07e141c4
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2021-07-22 12:42:15 +0200

    Update a1_p3.c

--- Assignment_1/a1_p3.c
+++ Assignment_1/a1_p3.c
@@ -41,27 +41,3 @@ return 0;
 // The result is a float, therefore the placeholder should reflect the same datatype.
 // Change %d to %f
 
-
-/*#include <stdio.h>
-
-int main() {
-	float result; /* The result of the division */
-	/*int a = 5;
-	
-	// Assigning a floating point value to an int results in the 
-	// value being truncated down to the nearest integer in this case 13
-	// To solve this, change the type of the variable to match the value
-	float b = 13.5;
-	
-	// Result has a floating point number as a value from
-	// the division between an int and a float
-	result = a / b;
-	
-	// Initial statement wanted to receive an integer data type as an argument
-	// Our result is a float data type
-	// To solve this, chnage the type that the printf statement is expecting to
-	// match our result data type
-	printf("The result is %f\n", result);
-	
-	return 0; */
-//}
